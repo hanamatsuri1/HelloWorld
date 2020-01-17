9e412189d973717b6524d9fb807692f64307f3bd
Jiahao Fu <jfu@bsu.edu>
2020-01-17 12:39:29 -0500

    CS222 Debug & Clone & PR

---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+package HelloWorld;
+
+//CS222
+//Jiahao Fu
+
+public class HelloWorld {
+
+    public static void main(String[] args) {  //Create a new main method
+
+        System.out.println("Hello again, World");  //Print "Hello again, World" to the screen
+
+    }  //close main
+
+}
